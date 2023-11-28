82bb377e41e00dd9187ade8830bc644615c0e52a
FIXWER <138541312+FIXWER@users.noreply.github.com>
2023-11-28 12:46:27 -0300

    Initial commit

---
+++ src/ProjetoImparPar/Projeto.java
@@ -0,0 +1,14 @@
+package ProjetoImparPar;
+
+public class Projeto {
+    public static void main(String[] args) {
+        int numero = 2;
+        if( (numero  % 2) == 0){
+            System.out.println("PAR:" + (numero % 2));
+
+        }else{
+            System.out.println("IMPAR:" + (numero % 2));
+        }
+    }
+}
+
